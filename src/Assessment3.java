078309f19a0eb19c8aabb2c18719fc31a85fe9a5
mohammed.hussain1508 <mohammed.hussain@veeva.com>
2024-08-29 16:45:09 +0530

    first commit

---
+++ src/Assessment3.java
@@ -0,0 +1,30 @@
+public class Assessment3 {
+
+    public static void SecondLargest() {
+
+        int[] arr = {6, 4, 11, 21, 12, 13, 6};
+        Integer largest = null;
+        Integer secondLargest = null;
+
+        for (int num : arr) {
+            if (largest == null || num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num != largest && (secondLargest == null || num > secondLargest)) {
+                secondLargest = num;
+            }
+        }
+        if (secondLargest != null) {
+            System.out.println("The second largest number is: " + secondLargest);
+        } else {
+            System.out.println("There is no second largest number.");
+        }
+
+
+    }
+}
+
+
+
+
+
